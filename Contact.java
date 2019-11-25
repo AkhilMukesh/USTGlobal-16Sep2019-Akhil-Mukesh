04c6ade33925ea862d4438d21606c223f72513e6
EchoIsHere <44767186+EchoIsHere@users.noreply.github.com>
2019-11-25 13:36:45 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,26 @@
+package com.ustglobal.mobileapp.dto;
+
+public class Contact {
+	String name;
+	long phno;
+	String groups;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	public String getGroups() {
+		return groups;
+	}
+	public void setGroups(String groups) {
+		this.groups = groups;
+	}
+
+}
